49ba76fd70082b1717fd1f8933d80edba5db783d
Pranav Murthy <pranavvmurthy26@gmail.com>
2017-01-02 02:45:38 -0500

    Create pattern_match.py

---
+++ pattern_match.py
@@ -0,0 +1,43 @@
+import copy
+import numpy as np
+
+
+def match_pattern(num_pattern, user_pattern):
+    result_list = []
+    num_pattern = np.asarray(num_pattern)
+    for i in num_pattern:
+        index_values = np.where(num_pattern == i)[0]
+        index_values = list(index_values)
+        for i, value in enumerate(index_values):
+            if i == 0:
+                temp = user_pattern[value]
+            else:
+                if temp == user_pattern[value]:
+                    pass
+                else:
+                    result_list.append(False)
+    return result_list
+
+
+def find_pattern(pattern):
+    dup_pattern = copy.deepcopy(pattern)
+    for i, n in enumerate(pattern):
+        letter = n
+        for num, val in enumerate(dup_pattern):
+            if val == letter:
+                dup_pattern[num]= i
+    return dup_pattern
+
+
+if __name__ == "__main__":
+    pattern = input("please enter the pattern in csv format:")
+    user_pattern = input("enter the pattern in csv format:")
+    pattern = pattern.split(",")
+    user_pattern = user_pattern.split(",")
+    num_pattern = find_pattern(pattern)
+    print(num_pattern)
+    ret = match_pattern(num_pattern, user_pattern)
+    if len(ret) == 0:
+        print("Pattern matches")
+    else:
+        print("Pattern does NOT match")
