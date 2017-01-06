d2bae9bdd65f3a4240d436d72c3561d7f9cc947f
Pranav Murthy <pranavvmurthy26@gmail.com>
2017-01-06 06:05:44 -0500

    Update folder_structure.py

--- folder_structure.py
+++ folder_structure.py
@@ -1,8 +1,8 @@
-example1 = "dir1\n\tdir11\n\tdir12\n\t\tpicture.jpeg\n\t\tdir121\n\t\t\tfile1.txt\n\t\tdir122\ndir2\n\tfile2.gif"
+example1 = "dir1\n\tdir11\n\tdir12\n\t\tpicture.jpg\n\t\tdir121\n\t\t\tfile1.txt\n\t\tdir122\ndir2\n\tfile2.gif"
 example2 = "dir1\n\tdir11\n\t\tpictures1.png\n\tdir12\n\t\tpicture.jpeg\n\t\tdir121\n\t\t\tfile1.txt\n\t\tdir122\n\tdir13\ndir2\n\tfile2.gif"
 example3 = "dir1\n\tdir11\n\t\tpictures1.png\n\tdir12\n\t\tpicture.jpeg\n\t\tdir121\n\t\t\tfile1.txt\n\t\t\tdir1211\n\t\t\t\tfile2.txt\n\t\tdir122\n\tdir13\ndir2\n\tfile2.gif"
 example4 = "dir1\n\tdir11\n\t\tpictures1.png\n\tdir12\n\t\tpicture.jpeg\n\tdir12A\n\t\tdir121\n\t\t\tfile1.txt\n\t\t\tdir1211\n\t\t\t\tfile2.txt\n\t\tdir122\n\tdir13\ndir2\n\tfile2.gif"
-
+example5 = "dir1\n\tdir11\n\tdir12\n\t\tpicture1.jpg\n\t\tpicture2.jpg\n\t\tdir121\n\t\t\tfile1.txt\n\t\tdir122\ndir2\n\tfile2.gif"
 
 def remove_white_space(_string):
     _string = _string.split("\t")
@@ -40,7 +40,6 @@ def build_dir_structure(file_system):
         elif cnt < cnt_old:
             folder_name = remove_white_space(_string=folder_name)
             length = len(open_folders)
-            # diff = length - cnt
             del open_folders[length - cnt]
             open_folders[cnt] = folder_name
             files_string.append(folder_name)
@@ -50,12 +49,27 @@ def build_dir_structure(file_system):
         file_system_structure.append(files_string)
     return file_system_structure
 
+def find_sum_of_folders(file_system_structure):
+    result = []
+    file_extentions = ['.jpg','.gif','.png', '.jpeg', '.mp4']
+    for _files in file_system_structure:
+        file_name = _files[0]
+        file_directory = _files[1]
+        for extention in file_extentions:
+            if extention in file_name:
+                result.append(len(list(file_directory)))
+
+    return sum(result)
+
+
 
 if __name__ == "__main__":
-    example = example4
+    example = example5
     file_system = example
     print(example)
     file_system_parsed = build_dir_structure(file_system)
+    for file_ in file_system_parsed:
+        print(file_)
+    sum_file_structure = find_sum_of_folders(file_system_structure=file_system_parsed)
+    print(sum_file_structure)
 
-    for individual_files in file_system_parsed:
-        print(individual_files)
